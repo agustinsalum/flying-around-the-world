cb3cd6a756e05509068b771bce39efd5a61bf13f
Agustin Salum <agustinsalum92@hotmail.com>
2023-09-27 23:21:25 -0300

    Realizado el formulario

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,25 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("contact-form");
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+function sendMail() {
+    const form_name = document.getElementById("name").value;
+    const form_email = document.getElementById("email").value;
+    const form_message = document.getElementById("message").value;
 
-        const nombre = document.getElementById("nombre").value;
-        const email = document.getElementById("email").value;
-        const mensaje = document.getElementById("mensaje").value;
+    if (!form_name || !form_email || !form_message) {
+        alert("Por favor, complete todos los campos.");
+        return;
+    }
 
-        if (!nombre || !email || !mensaje) {
-            alert("Por favor, complete todos los campos.");
-            return;
-        }
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
 
-        alert(nombre);
-        alert(email);
-        alert(mensaje);
+    // Deben ingresar el serviceID y templateID
+    const serviceID = 'service_yxvpl47';
+    const templateID = 'template_8ldfder';
 
-        const subject = `Mensaje de ${nombre}`;
-        const mailtoLink = `mailto:correo_destino@example.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(mensaje)}`;
+    emailjs.send(serviceID, templateID, params).then(function (res) {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+    })
+    alert("Su mensaje se envio con exito!!")
+    .catch (err => console.log(err));
 
-        window.location.href = mailtoLink;
-    });
-});
+}
