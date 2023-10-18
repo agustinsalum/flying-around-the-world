2243b0152a59d4f16147a3fe6e9ac407fd38b8a4
Agustin Salum <agustinsalum92@hotmail.com>
2023-10-18 11:39:50 -0300

    Agregada animacion al menu hamburguesa

---
+++ assets/js/animation.js
@@ -0,0 +1,6 @@
+// https://es.javascript.info/css-animations
+var mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenu.onclick = function() {
+    mobileMenu.classList.toggle('menu-clicked');
+};
