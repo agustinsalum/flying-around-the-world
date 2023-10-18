56df9222839328d81a0f193c4e73556ebb315444
Agustin Salum <agustinsalum92@hotmail.com>
2023-10-18 12:25:02 -0300

    Terminando

--- assets/js/animation.js
+++
@@ -1,6 +0,0 @@
-// https://es.javascript.info/css-animations
-var mobileMenu = document.getElementById('mobile-menu');
-
-mobileMenu.onclick = function() {
-    mobileMenu.classList.toggle('menu-clicked');
-};
