2a20d928eeb87226dafe68ce75422239a4f60f2e
Agustin Salum <agustinsalum92@hotmail.com>
2023-12-07 17:08:34 -0300

    Estilos en tabla y solucion de enlaces

--- fly/urls.py
+++ fly/urls.py
@@ -6,9 +6,9 @@ from .views import FlyIndexViews, FlyFormViews, FlyFlightsViews, FlyAboutUsViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',FlyIndexViews.as_view(), name = 'index'),
-    path('index.html',FlyIndexViews.as_view(), name = 'index'),
-    path('form.html',FlyFormViews.as_view(), name = 'form'),
-    path('flights.html',FlyFlightsViews.as_view(), name = 'flights'),
-    path('about_us.html',FlyAboutUsViews.as_view(), name = 'abouts_us'),
+    path('index/',FlyIndexViews.as_view(), name = 'index'),
+    path('form/',FlyFormViews.as_view(), name = 'form'),
+    path('flights/',FlyFlightsViews.as_view(), name = 'flights'),
+    path('about_us/',FlyAboutUsViews.as_view(), name = 'abouts_us'),
     path("trips/", include("flyApp.urls")),
 ]
